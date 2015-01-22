65c18dcb8b8a83e62a4a76a3f2b0ad77f6dcfc6b
Kane Baccigalupi <baccigalupi@gmail.com>
2015-01-21 20:08:47 -0800

    cleaning up the main.c file

--- lib/parser/main.c
+++ lib/parser/main.c
@@ -12,19 +12,7 @@ int main() {
   while(!context || fxp_parser_context_status(context)) {
     context = parse_stdin();
     verify(context);
-
-    if ( fxp_parser_context_status(context) ) {
-      printf("%s", string_value(fxp_parser_context_error_message(context)));
-    } else {
-      String *inspection = fxp_parser_inspect(context);
-      verify(inspection);
-      printf("%s", string_value(inspection));
-      string_free(inspection);
-    }
-
     fxp_parser_context_free(context);
-
-    printf("\n\n");
   }
 
   return 0;
