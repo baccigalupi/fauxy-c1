63ece0686669303291ccc1c3745e001930a1507c
Kane Baccigalupi <baccigalupi@gmail.com>
2015-01-23 09:08:21 -0800

    class fixes from spike on class organization

--- lib/native/class.h
+++ lib/native/class.h
@@ -1,16 +1,15 @@
 #ifndef FxN_Class_h
 #define FxN_Class_h 1
 
-#include "../bricks/list.h"
 #include "../bricks/string.h"
 
 typedef struct FxN_Class {
   FxB_String  *name;
-  FxB_List    *ancestors;
+  struct FxN_Class *ancestor;
 } FxN_Class;
 
 #define fxn_class_name(C)       ((C)->name)
-#define fxn_class_ancestors(C)  ((C)->ancestors)
+#define fxn_class_ancestor(C)   ((C)->ancestor)
 #define fxn_class_free(C)       (fx_pfree(C)) // TODO: more better
 
 #endif
