71b9317063dd44a354b396122d0aba86b51eef88
Kane Baccigalupi <baccigalupi@gmail.com>
2015-01-26 12:00:54 -0800

    fixes copy/paste error with interpreter function definition

--- lib/interpreter/class.h
+++ lib/interpreter/class.h
@@ -1,8 +1,8 @@
 #ifndef FxN_Class_h
 #define FxN_Class_h 1
 
+#include "../bricks/helpers.h"
 #include "../bricks/hash_map.h"
-#include "function.h"
 
 typedef struct FxN_Class {
   char             *name;
@@ -10,13 +10,15 @@ typedef struct FxN_Class {
   FxB_HashMap      *method_groups;
 } FxN_Class;
 
+#include "function.h"
+
 #define fxn_class_name(C)          ((C)->name)
 #define fxn_class_super_class(C)   ((C)->super_class)
 #define fxn_class_free(C)          (fx_pfree(C)) // TODO: more better
 #define fxn_class_method_groups(C) ((C)->method_groups)
 
 FxN_Class    *FxN_Class_create(char *name, FxN_Class *super_class);
-void          fxn_class_set_method(FxN_class *self, char *method_name, FxN_Function *function);
-FxN_Function *fxn_class_get_method(FxN_class *self, char *method_name);
+void          fxn_class_set_method(FxN_Class *self, char *method_name, FxN_Function *function);
+FxN_Function *fxn_class_get_method(FxN_Class *self, char *method_name);
 
 #endif
